478f35e7c5c0e2d87707b7a0b4c02682638137e1
yguntur <163242018+yguntur@users.noreply.github.com>
2025-04-18 18:25:46 +0530

    Update streamlit_app.py
    Update search_on in the for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,18 @@ st.write(
   """Choose the fruits you want in your custom smoothie.
   """
 )
+
+name_on_order = st.text_input("Name on the smoothie")
+st.write("The name on the smoothie is: ", name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()    
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     "Choose upto five ingredinets", 
@@ -28,15 +32,17 @@ ingredients_list = st.multiselect(
 #st.write("You selected:", Ingredients_list)
 if ingredients_list:
     ingredients_string = ''
-    for i in ingredients_list:
+    for i in ingredients_list:        # here 'i' is 'fruit_chosen'
         ingredients_string += i + " "
+      
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', i,' is ', search_on, '.')
+      
         st.subheader( i + 'Nutrition_information' )
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + i)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string )
 
-name_on_order = st.text_input("Name on the smoothie")
-st.write("The name on the smoothie is: ", name_on_order)
 
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
