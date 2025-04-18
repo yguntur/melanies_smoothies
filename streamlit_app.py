37bf419369cc498687810cd5c77212aef0073832
yguntur <163242018+yguntur@users.noreply.github.com>
2025-04-18 17:01:46 +0530

    Update streamlit_app.py
    add json to smoothiefroot_response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if time_to_submit:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
